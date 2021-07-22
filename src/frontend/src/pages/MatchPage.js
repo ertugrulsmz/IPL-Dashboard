0b017112900451dd8e1a036889d2985efad725e2
ertugrulsmz <ertugrulsmz55@gmail.com>
2021-07-22 14:17:23 +0300

    match page draft

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -5,11 +5,36 @@ import {useParams} from 'react-router-dom'
 
 export const MatchPage = () => {
 
+    const [matches,setMatches] = useState([]);
+
+
+    const {teamName,year} = useParams();
+
+
+    useEffect(() => {
+            const fetchMatches = async () => {
+                //fetch returns promise
+                const response = await
+                    fetch(`http://localhost:8085/teams/${teamName}/matches/?year=${year}`);
+                const data = await response.json();
+                console.log(data);
+                setMatches(data);
+            };
+
+            fetchMatches();
+        },[] //when Link is clicked path so teamname changes
+        // it does not send request to server just change the path
+        // when path changes you fetch again
+    );
 
     return (
         <div className="MatchPage">
             <h1>Match Page</h1>
 
+            {
+                matches.map(match =>
+                    <MatchDetailCard key={match.id} teamName={teamName} match={match}/>)
+            }
 
         </div>
     );
